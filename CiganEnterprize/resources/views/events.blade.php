63622585936657b20987847d003dbd7c1301f02a
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-09-01 21:54:42 +0300

    Update CiganEnterprize

---
+++ CiganEnterprize/resources/views/events.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Cigan Enterprize</title>
+  </head>
+  <body>
+  <div class="navbar">
+      <a href="/">Home</a>
+      <a href="/events">Events</a>
+      <a href="/industries">Industries</a>
+      <a href="/jobs">Jobs</a>
+      <a href="/sediums">Sediums</a>
+      <a href="/blog">Blog</a>
+      <a href="/contact">Contact</a>
+    </div>
+    <h1>Events</h1>
+  </body>
+</html>
