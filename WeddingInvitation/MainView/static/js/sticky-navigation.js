66997ab64de0437fa26f78b291602acaae759062
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-09-03 23:27:33 +0300

    Update wedding-app

--- WeddingInvitation/MainView/static/js/sticky-navigation.js
+++ WeddingInvitation/MainView/static/js/sticky-navigation.js
@@ -1,38 +1,8 @@
-// Sticky Header
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 12) {
-    $(".header").addClass("sticky");
-  } else {
-    $(".header").removeClass("sticky");
-  }
-});
+const toggleButton = document.querySelector('.toggle-menu');
 
-// Mobile Navigation
-$(".mobile-toggle").click(function() {
-  if ($(".header").hasClass("open-nav")) {
-    $(".header").removeClass("open-nav");
-  } else {
-    $(".header").addClass("open-nav");
-  }
-});
+const navBar = document.querySelector('.nav-bar');
 
-$(".header li a").click(function() {
-  if ($(".header").hasClass("open-nav")) {
-    $(".navigation").removeClass("open-nav");
-    $(".header").removeClass("open-nav");
-  }
-});
-
-// Navigation Scroll
-$("nav a").click(function(event) {
-  var id = $(this).attr("href");
-  var offset = 70;
-  var target = $(id).offset().top - offset;
-  $("html, body").animate(
-    {
-      scrollTop: target
-    },
-    500
-  );
-  event.preventDefault();
+toggleButton.addEventListener('click', () => {
+  
+  navBar.classList.toggle('toggle');
 });
