1dc7bb9c768f1268db81b3e5ca1e0767f2bf2864
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-09-03 23:41:08 +0300

    Update portofolio

--- WeddingInvitation/WeddingInvitation/settings.py
+++ WeddingInvitation/WeddingInvitation/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@##=lw7x^i_tw6ycu&o!6f6fc&8*k&3+cg9b%4y4z9+r*k^hpd'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
