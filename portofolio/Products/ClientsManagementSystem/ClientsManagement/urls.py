d3441bc2eab7918f2fd1bcb2664ae725818b86ca
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-10-23 15:43:50 +0300

    Delete Portofolio

--- portofolio/Products/ClientsManagementSystem/ClientsManagement/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('feedback/', views.FeedBackPage.as_view(), name = 'feedback_page'),
-    path('ask-project/', views.AskProjectPage.as_view(), name = 'ask_project'),
-    path('client-contact/', views.ClientContactPage.as_view(), name = 'client_contact'),
-]
