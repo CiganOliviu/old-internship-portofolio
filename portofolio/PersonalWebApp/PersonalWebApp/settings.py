3e2363d5fa0bcbffd1f5d8e9f42251a405459074
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-10-25 12:13:34 +0200

    Update Portofolio

--- portofolio/PersonalWebApp/PersonalWebApp/settings.py
+++ portofolio/PersonalWebApp/PersonalWebApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '((dm*$pt^q3esy6^ha&w(ei##lz9vt8p0^m$(rq1#6p1brppbz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
