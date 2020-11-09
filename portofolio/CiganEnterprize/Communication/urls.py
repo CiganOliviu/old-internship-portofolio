ea9973015c0a225a480bed0985fb022ee3a6ee86
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-11-09 10:52:06 +0200

    ERASE

--- portofolio/CiganEnterprize/Communication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('newsletter/', views.NewsletterView.as_view(), name='newsletter'),
-    path('contact/', views.FormularContact.as_view(), name='formular_contact'),
-]
