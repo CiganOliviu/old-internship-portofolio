52e4bd9999360a4b00d25c778e0733489211d9cc
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-12-26 20:52:35 +0200

    Update CMS Plugin

--- Products/ClientsManagementSystemDjangoPlugin/ClientsManagement/views.py
+++ Products/ClientsManagementSystemDjangoPlugin/ClientsManagement/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
 from django.views import generic
 
 from ClientsManagement.models import (
@@ -40,9 +38,21 @@ from Index.client_projects_relationship import (
 )
 
 
+def set_args_for_index(active_working_projects_query, client_details_query, finished_projects_query,
+                       planned_projects_query, projects_request_query):
+    args = {
+        'client_details_query': client_details_query,
+        'active_working_projects_query': active_working_projects_query,
+        'finished_projects_query': finished_projects_query,
+        'planned_projects_query': planned_projects_query,
+        'projects_request_query': projects_request_query,
+    }
+
+    return args
+
+
 @login_required
 def index(request):
-
     template_name = 'views/client_index_view.html'
 
     client_details_query = ClientsDetail.objects.filter(user=request.user)[:3]
@@ -51,13 +61,8 @@ def index(request):
     planned_projects_query = PlannedProject.objects.filter(user=request.user)[:3]
     projects_request_query = ProjectsRequest.objects.filter(user=request.user)[:3]
 
-    args = {
-        'client_details_query': client_details_query,
-        'active_working_projects_query': active_working_projects_query,
-        'finished_projects_query': finished_projects_query,
-        'planned_projects_query': planned_projects_query,
-        'projects_request_query': projects_request_query,
-    }
+    args = set_args_for_index(active_working_projects_query, client_details_query, finished_projects_query,
+                              planned_projects_query, projects_request_query)
 
     create_user_in_client_detail(request.user)
 
@@ -73,66 +78,82 @@ def index(request):
     return render(request, template_name, args)
 
 
+def set_args_for_active_working_projects(active_working_projects_query, client_details_query):
+    args = {
+        'client_details_query': client_details_query,
+        'active_working_projects_query': active_working_projects_query,
+    }
+    return args
+
+
 @login_required
 def active_working_projects(request):
-
     template_name = 'views/projects/active_working_projects.html'
 
     client_details_query = ClientsDetail.objects.filter(user=request.user)
     active_working_projects_query = ActiveWorkingProject.objects.filter(user=request.user)
 
-    args = {
-        'client_details_query': client_details_query,
-        'active_working_projects_query': active_working_projects_query,
-    }
+    args = set_args_for_active_working_projects(active_working_projects_query, client_details_query)
 
     return render(request, template_name, args)
 
 
+def set_args_for_planned_projects(client_details_query, planned_projects_query):
+    args = {
+        'client_details_query': client_details_query,
+        'planned_projects_query': planned_projects_query,
+    }
+    return args
+
+
 @login_required
 def planned_projects(request):
-
     template_name = 'views/projects/planned_projects.html'
 
     client_details_query = ClientsDetail.objects.filter(user=request.user)
     planned_projects_query = PlannedProject.objects.filter(user=request.user)
 
-    args = {
-        'client_details_query': client_details_query,
-        'planned_projects_query': planned_projects_query,
-    }
+    args = set_args_for_planned_projects(client_details_query, planned_projects_query)
 
     return render(request, template_name, args)
 
 
+def set_args_for_finished_projects(client_details_query, finished_projects_query):
+    args = {
+        'client_details_query': client_details_query,
+        'finished_projects_query': finished_projects_query,
+    }
+    return args
+
+
 @login_required
 def finished_projects(request):
-
     template_name = 'views/projects/finished_projects.html'
 
     client_details_query = ClientsDetail.objects.filter(user=request.user)
     finished_projects_query = FinishedProject.objects.filter(user=request.user)
 
-    args = {
-        'client_details_query': client_details_query,
-        'finished_projects_query': finished_projects_query,
-    }
+    args = set_args_for_finished_projects(client_details_query, finished_projects_query)
 
     return render(request, template_name, args)
 
 
+def set_args_for_requested_projects(client_details_query, requested_projects_query):
+    args = {
+        'client_details_query': client_details_query,
+        'requested_projects_query': requested_projects_query,
+    }
+    return args
+
+
 @login_required
 def requested_projects(request):
-
     template_name = 'views/projects/requested_projects.html'
 
     client_details_query = ClientsDetail.objects.filter(user=request.user)
     requested_projects_query = ProjectsRequest.objects.filter(user=request.user)
 
-    args = {
-        'client_details_query': client_details_query,
-        'requested_projects_query': requested_projects_query,
-    }
+    args = set_args_for_requested_projects(client_details_query, requested_projects_query)
 
     return render(request, template_name, args)
 
@@ -141,9 +162,25 @@ class UpdateProfile(LoginRequiredMixin, generic.TemplateView):
     template_name = 'views/update_profile/update_profile_form.html'
     final_template_name = 'views/update_profile/update_profile_form_done.html'
 
+    def _set_default_values_for_user_detail_form(self, form, get_query_for_default):
+        form.fields['first_name'].initial = get_query_for_default.first_name
+        form.fields['last_name'].initial = get_query_for_default.last_name
+        form.fields['client_description'].initial = get_query_for_default.client_description
+        form.fields['email'].initial = get_query_for_default.email
+        form.fields['phone_number'].initial = get_query_for_default.phone_number
+        form.fields['website'].initial = get_query_for_default.website
+        form.fields['social_media_presence'].initial = get_query_for_default.social_media_presence
+        form.fields['profile_image'].initial = get_query_for_default.profile_image
+        form.fields['type_of_client'].initial = get_query_for_default.type_of_client
+
     def get(self, request):
 
         form = ClientsDetailForm()
+
+        get_query_for_default = ClientsDetail.objects.get(user=request.user)
+
+        self._set_default_values_for_user_detail_form(form, get_query_for_default)
+
         post = form.save(commit=False)
 
         args = {'form': form}
@@ -153,7 +190,6 @@ class UpdateProfile(LoginRequiredMixin, generic.TemplateView):
     def _replace_user_details(self, database_object):
 
         if ClientsDetail.objects.filter(user=database_object.user).exists():
-
             process_query = ClientsDetail.objects.get(user=database_object.user)
 
             process_query.profile_image = database_object.profile_image
@@ -172,7 +208,6 @@ class UpdateProfile(LoginRequiredMixin, generic.TemplateView):
         form = ClientsDetailForm(request.POST, request.FILES)
 
         if form.is_valid():
-
             post = form.save(commit=False)
             post.user = request.user
 
@@ -182,7 +217,6 @@ class UpdateProfile(LoginRequiredMixin, generic.TemplateView):
 
 
 class ClientsFeedBackPage(LoginRequiredMixin, generic.TemplateView):
-
     template_name = 'views/clients_feedback/feedback.html'
     final_template_name = 'views/clients_feedback/feedback_final.html'
 
@@ -219,7 +253,6 @@ class ClientsFeedBackPage(LoginRequiredMixin, generic.TemplateView):
 
 
 class ProjectsRequestPage(LoginRequiredMixin, generic.TemplateView):
-
     template_name = 'views/projects_requests/project_request_perform.html'
     final_template_name = 'views/projects_requests/project_request_perform_done.html'
 
@@ -259,7 +292,6 @@ class ProjectsRequestPage(LoginRequiredMixin, generic.TemplateView):
 
 @login_required()
 def client_messages(request):
-
     template_name = 'views/client_messages/messages_page.html'
 
     messages_query = MessagesToClient.objects.filter(user=request.user)
@@ -271,7 +303,6 @@ def client_messages(request):
 
 @login_required()
 def all_messages(request):
-
     template_name = 'views/client_messages/all_messages_page.html'
 
     messages_query = MessagesToClient.objects.filter(user=request.user)[:5]
@@ -283,7 +314,6 @@ def all_messages(request):
 
 @login_required()
 def projects_messages(request):
-
     template_name = 'views/client_messages/projects_messages_page.html'
 
     messages_query = MessagesToClient.objects.filter(user=request.user, type_of_message='Projects Reference')
@@ -295,7 +325,6 @@ def projects_messages(request):
 
 @login_required()
 def workflow_messages(request):
-
     template_name = 'views/client_messages/workflow_messages_page.html'
 
     messages_query = MessagesToClient.objects.filter(user=request.user, type_of_message='Workflow Reference')
@@ -307,7 +336,6 @@ def workflow_messages(request):
 
 @login_required()
 def other_messages(request):
-
     template_name = 'views/client_messages/other_messages_page.html'
 
     messages_query = MessagesToClient.objects.filter(user=request.user, type_of_message='Others')
@@ -318,7 +346,6 @@ def other_messages(request):
 
 
 class MessageDetail(LoginRequiredMixin, generic.DetailView):
-
     model = MessagesToClient
 
     template_name = 'views/client_messages/message_detail.html'
@@ -328,7 +355,6 @@ class MessageDetail(LoginRequiredMixin, generic.DetailView):
 
 
 class RequestedProjectDetail(LoginRequiredMixin, generic.DetailView):
-
     model = ProjectsRequest
 
     template_name = 'views/projects_detail/requested_project_detail.html'
