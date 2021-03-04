c7fbbeb4f8935f273200d4bcb819f3716c53f848
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-03-04 20:09:03 +0200

    Setup InfiniteShoot/Appointment models

--- OutSourcing/InfiniteShoot/Appointment/admin.py
+++ OutSourcing/InfiniteShoot/Appointment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Appointment, AdminResponse
 
-# Register your models here.
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'calendar', 'sent_moment')
+
+
+class AdminResponseAdmin(admin.ModelAdmin):
+    list_display = ('client', 'appointment', 'location', 'response_moment')
+
+
+admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(AdminResponse, AdminResponseAdmin)
--- OutSourcing/InfiniteShoot/Appointment/models.py
+++ OutSourcing/InfiniteShoot/Appointment/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Appointment(models.Model):
+    first_name = models.CharField(max_length=200, blank=False)
+    last_name = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(max_length=200, blank=False)
+    phone_number = models.CharField(max_length=10, blank=False)
+    calendar = models.DateField()
+    sent_moment = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
+
+class AdminResponse(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+    appointment = models.DateField()
+    location = models.CharField(max_length=100, blank=False)
+    response_moment = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.client
+
